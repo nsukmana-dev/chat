6441e0c4c0fdc35b09e539a4cf131966530fa58a
Nandar Sukmana <nsukmana.dev@gmail.com>
2022-07-08 08:25:45 +0700

    setup username before chat

--- server.js
+++ server.js
@@ -6,9 +6,25 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html')
 });
 
+var users = {};
+var usernames = [];
+
 io.on('connection', function(socket) {
 
     socket.broadcast.emit('newMessage', 'someone join chat');
+
+    socket.on('registerUser', function(username){
+        if (usernames.indexOf(username) != -1) {
+            socket.emit('registerRespond', false);
+        }else{
+            users[socket.id] = username;
+            usernames.push(username);
+            socket.emit('registerRespond', true);
+            console.log(users);
+            console.log("========");
+            console.log(usernames);
+        }
+    });
 
     socket.on('newMessage', function(msg) {
         io.emit('newMessage', msg);
